9dde1d7fd9640e70b0fa711517fe85279dd1dd36
ravibp <ravi.prasanna@infosys.com>
2019-12-04 03:46:09 +0530

    Updates

---
+++ precache-manifest.c5501b19c648c1dae8b9746f7bb44d92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a48f55e44bcdb7798af95a2d445b2624",
+    "url": "/react-assignment/index.html"
+  },
+  {
+    "revision": "b81a08ad40ac2be49ca4",
+    "url": "/react-assignment/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "ac8ed750f622b77fbdd2",
+    "url": "/react-assignment/static/css/main.cea6a0b9.chunk.css"
+  },
+  {
+    "revision": "b81a08ad40ac2be49ca4",
+    "url": "/react-assignment/static/js/2.c49a61f6.chunk.js"
+  },
+  {
+    "revision": "ac8ed750f622b77fbdd2",
+    "url": "/react-assignment/static/js/main.ee95959c.chunk.js"
+  },
+  {
+    "revision": "ca2eb1843738447aea59",
+    "url": "/react-assignment/static/js/runtime-main.329e22af.js"
+  }
+]);
