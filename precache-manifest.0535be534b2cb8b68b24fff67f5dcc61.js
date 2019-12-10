33e4d085a7ff31960530e026067ddd4de493fb93
ravibp <ravi.prasanna@searshc.com>
2019-12-10 15:40:13 +0530

    Updates

---
+++ precache-manifest.0535be534b2cb8b68b24fff67f5dcc61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0488ee7f2551064665fcf90f8ac16220",
+    "url": "/excelsheet-extractor/index.html"
+  },
+  {
+    "revision": "bf6da9cdb801f469fcb5",
+    "url": "/excelsheet-extractor/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "b1c5ae2ea0c8b041c0c5",
+    "url": "/excelsheet-extractor/static/css/main.16921805.chunk.css"
+  },
+  {
+    "revision": "bf6da9cdb801f469fcb5",
+    "url": "/excelsheet-extractor/static/js/2.04244b56.chunk.js"
+  },
+  {
+    "revision": "b1c5ae2ea0c8b041c0c5",
+    "url": "/excelsheet-extractor/static/js/main.ceba2d04.chunk.js"
+  },
+  {
+    "revision": "4143fb7a11509122c180",
+    "url": "/excelsheet-extractor/static/js/runtime-main.39c37ccc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-assignment/precache-manifest.ab307f16e7b1d727e264e84b7e53d761.js"
+  "/excelsheet-extractor/precache-manifest.0535be534b2cb8b68b24fff67f5dcc61.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-assignment/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/excelsheet-extractor/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
